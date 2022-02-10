2ecedaea905bfe3bd73debce19abedf3f8ce60cf
Ivan <v.271998@yandex.ru>
2022-02-10 13:54:15 +0300

    fixes in environment file

--- high_level_control/main.py
+++ high_level_control/main.py
@@ -1,4 +1,5 @@
- 
+#!/usr/bin/env python3
+
 import time
 import random
 from utils import AttemptCounter
@@ -11,7 +12,6 @@ from functions import check_all_nodes, run_segmentation, move, moveManipulatorTo
 import config as cfg
 
 counter = AttemptCounter()
-
 
 
 class InitializeAll(smach.State):
@@ -59,16 +59,18 @@ class RecognizeObjects(smach.State):
 
         userdata.mask = mask
         userdata.class_name = cl
-        
+
         if len(mask.data) == 0:
             return 'No objects found'
         # else:
             # userdata.depth_masked, userdata.mask, cl, confidence, dist = ret
         elif confidence > cfg.conf_thresh and dist < cfg.dist_thresh and cl != '':
-            print(f'Found object: {cl} with confidence {confidence:.2f}, distance: {dist:.2f}')
+            print(
+                f'Found object: {cl} with confidence {confidence:.2f}, distance: {dist:.2f}')
             return 'Object found'
         else:
-            print(f'Found object of class {cl}, confidence: {confidence:.2f}, distance: {dist:.2f}')
+            print(
+                f'Found object of class {cl}, confidence: {confidence:.2f}, distance: {dist:.2f}')
             return 'Object found with low confidence'
 
 
@@ -134,7 +136,8 @@ class LearnNewObject(smach.State):
     def __init__(self, outcomes=['Object saved', 'Object not saved', 'Grasps not generated'],
                  input_keys=['depth_masked'],
                  output_keys=['depth_masked']):
-        smach.State.__init__(self, outcomes=outcomes, input_keys=input_keys, output_keys=output_keys)
+        smach.State.__init__(self, outcomes=outcomes,
+                             input_keys=input_keys, output_keys=output_keys)
 
     def execute(self, userdata):
 
@@ -148,53 +151,50 @@ if __name__ == '__main__':
 
     rospy.init_node('blablabla')
 
-    
     sm = smach.StateMachine(outcomes=['Stopped'])
 
     sis = smach_ros.IntrospectionServer('server_name', sm, '/SM_ROOT')
     sis.start()
 
-
-
     with sm:
         smach.StateMachine.add('INITIALIZATION', InitializeAll(),
-                            transitions={
+                               transitions={
             'All initialized': 'WAIT FOR COMMAND',
             'Not all initialized': 'Stopped'
         })
         smach.StateMachine.add('WAIT FOR COMMAND', WaitForCommand(),
-                            transitions={
+                               transitions={
             'Start working': 'RECOGNIZE OBJECTS'
         })
 
         smach.StateMachine.add('RECOGNIZE OBJECTS', RecognizeObjects(),
-                            transitions={
+                               transitions={
             'Object found': 'GENERATE GRASPS',
             'Object found with low confidence': 'LEARN OBJECT',
             'No objects found': 'CHANGE POSITION',
         })
 
         smach.StateMachine.add('GENERATE GRASPS', GenerateGrasps(),
-                            transitions={
+                               transitions={
             'Grasps generated': 'PLAN AND MOVE',
             'Grasps not generated': 'CHANGE POSITION',
         })
 
         smach.StateMachine.add('PLAN AND MOVE', PlanAndMove(),
-                            transitions={
+                               transitions={
             'Planning failed': 'CHANGE POSITION',
             'Moving sucessful': 'RECOGNIZE OBJECTS',
             'Moving sucessful': 'GO TO INITIAL STATE'
         })
 
         smach.StateMachine.add('GO TO INITIAL STATE', ReturnToInitState(),
-                            transitions={
+                               transitions={
             'Executed': 'WAIT FOR COMMAND',
             'Not executed': 'Stopped'
         })
 
         smach.StateMachine.add('CHANGE POSITION', ChangePosition(),
-                            transitions={
+                               transitions={
             'Position changed': 'RECOGNIZE OBJECTS',
             'Too many attempts': 'GO TO INITIAL STATE',
             # 'Moving failed': 'Stopped'
@@ -202,7 +202,7 @@ if __name__ == '__main__':
         })
 
         smach.StateMachine.add('LEARN OBJECT', LearnNewObject(),
-                            transitions={
+                               transitions={
             'Object saved': 'RECOGNIZE OBJECTS',
             'Object not saved': 'GO TO INITIAL STATE',
             'Grasps not generated': 'CHANGE POSITION'
@@ -216,6 +216,3 @@ if __name__ == '__main__':
     # rospy.spin()
 
     # sis.stop()
-
-
-
